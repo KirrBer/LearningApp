e8d476209551827f6c2c6c043d2a51aba68791f2
KirrBergOOO <operax0@yandex.ru>
2026-02-19 21:48:28 +0300

    added dao for work with db

--- services/skill_analyzer/routes.py
+++ services/skill_analyzer/routes.py
@@ -1,40 +1,19 @@
 from fastapi import APIRouter, Body
 from db import async_session_maker
-import pickle
-import spacy
-import json
+from dao import SkillDAO
+from utils import extract_skills
 
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-from torch import no_grad
 
 router = APIRouter()
 
 @router.post("/", summary="Получить список навыков")
 async def get_skills(data=Body()):
-    with open("data", "rb") as file:
-        all_skills = pickle.load(file)
-    nlp = spacy.load("./extractor_model/model-best")
-    doc = nlp(data['text'])
-    possible_skills = set()
-    for ent in doc.ents:
-        item = ent.text
-        if "\n" in item:
-            item = item.replace("\n", " ")
-        possible_skills.add(item)
-    skills = set()
-    for skill in possible_skills:
-        if skill.lower() in all_skills:
-            skills.add(skill)
-    def answer(x, **kwargs):
-        inputs = tokenizer(x, return_tensors='pt').to(model.device)
-        with no_grad():
-            hypotheses = model.generate(**inputs, **kwargs)
-        return tokenizer.decode(hypotheses[0], skip_special_tokens=True)
-    model = T5ForConditionalGeneration.from_pretrained("cointegrated/rut5-small")
-    tokenizer = T5Tokenizer.from_pretrained("cointegrated/rut5-small")
-    model = model.from_pretrained("./normalize_model")
-    tokenizer = tokenizer.from_pretrained("./normalize_model")
-    model.eval()
-    skills = list(skills)
-    normalized_skills = list(set([answer(skill) for skill in skills]))
-    return json.dumps({"message": normalized_skills})
+    skills = extract_skills(data['text'])
+    response = []
+    for skill in skills:
+        found = SkillDAO.find_skill(skill)
+        if found:
+            response.append(found[0])
+        else:
+            response.append({"name": skill, "course": None})
+    return response

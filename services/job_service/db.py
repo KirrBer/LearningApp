5920d9c6a51517e0a342f962767bb97968760258
KirrBergOOO <operax0@yandex.ru>
2026-03-17 16:26:59 +0300

    added postgres and seed_data to job_service

---
+++ services/job_service/db.py
@@ -0,0 +1,53 @@
+"""База данных и ORM-конфигурация.
+
+Здесь определяется:
+- асинхронный SQLAlchemy движок
+- декоратор `connection` для управления сессией
+- базовые типы для моделей (int_pk, str_uniq и т.д.)
+- базовый класс модели `Base` с автоматическим табличным именем.
+"""
+
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+from job_service.settings import settings
+
+
+DATABASE_URL = settings.get_db_url()
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+def connection(method):
+    """Декоратор для выполнения функции в контексте DB-сессии.
+
+    Позволяет не передавать сессию явно, упрощает работу с async SQLAlchemy.
+    """
+
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                # Явно не открываем транзакции, так как они уже есть в контексте
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()  # Откатываем сессию при ошибке
+                raise e  # Поднимаем исключение дальше
+            finally:
+                await session.close()  # Закрываем сессию
+
+    return wrapper
+
+
+# Базовые типы для моделей (для удобства и единообразия).
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
---
+++ services/job_service/seed_data.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+from job_service.settings import settings
+from job_service.models import Vacancy
+from job_service.db import connection
+from sqlalchemy import select
+from api_hhru import get_vacancies_id, get_vacancy
+
+
+logger = logging.getLogger(__name__)
+
+DATABASE_URL = settings.get_db_url()
+
+@connection
+async def seed_skills_with_models(session):
+    """Заполнение навыков используя модели SQLAlchemy"""
+    ids = get_vacancies_id()
+    for id in ids:
+        vacancy_data = get_vacancy(id)
+        query = select(Vacancy).where(Vacancy.name == vacancy_data["name"])
+        result = await session.execute(query)
+        existing = result.scalar_one_or_none()
+        if not existing:
+            vacancy = Vacancy(**vacancy_data)
+            session.add(vacancy)
+            logger.info(f"✅ Добавлен навык: {vacancy_data['name']}")
+        else:
+            existing.course = vacancy_data["course"]
+            logger.info(f"🔄 Обновлен навык: {vacancy_data['name']}")
